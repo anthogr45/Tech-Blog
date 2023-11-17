e72cd93bb6b0e2bc0f7ae15b0af4d546e97d1647
AnthoG <anthony.dswg@gmail.com>
2023-11-16 22:38:44 -0500

    HTML and few Styling

---
+++ public/js/signup.js
@@ -0,0 +1,9 @@
+function showSignUpForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('login-form').style.display = 'block';
+    document.getElementById('signup-form').style.display = 'none';
+}
